1ad51e01c1c548a30de2f41a7e848680848ba406
acf0x <alcasfer96@gmail.com>
2024-05-14 13:56:08 +0200

    final 14/05

--- Python/05-Microservicios/05.02-CP.py
+++ Python/05-Microservicios/05.02-CP.py
@@ -1,5 +1,5 @@
 #####################################################################
-# Trabajando con Request                                            #
+# Código postal ejemplo                                             #
 #####################################################################
 
 # Importar los módulos necesarios
@@ -16,14 +16,14 @@ try:
 
     if(response.status_code == 200):
         data = response.json()
-        for resultado in data["places"]:
+        for subdata in data["places"]:
             print(f"País: {data["country"]}")
-            print(f"Ubicación: {resultado["place name"]}")
-            print(f"Latitud: {resultado["latitude"]}")
-            print(f"Longitud: {resultado["longitude"]}")
-            print(f"Provincia: {resultado["state"]}")
-            print(f"Abreviación provincia: {resultado["state abbreviation"]}")
-            print(f"Latitud: {resultado["latitude"]}")
+            print(f"Ubicación: {subdata["place name"]}")
+            print(f"Latitud: {subdata["latitude"]}")
+            print(f"Longitud: {subdata["longitude"]}")
+            print(f"Provincia: {subdata["state"]}")
+            print(f"Abreviación provincia: {subdata["state abbreviation"]}")
+            print(f"Latitud: {subdata["latitude"]}")
             print()
     else:
         print(f"ERROR ({response.status_code}: {response.reason}\n")   
