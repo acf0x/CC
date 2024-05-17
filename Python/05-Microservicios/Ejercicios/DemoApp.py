2af9bec48c6110c16762e7f3042135f32675dfc6
acf0x <alcasfer96@gmail.com>
2024-05-17 13:55:43 +0200

    "Added new API endpoints for orders and customer orders, and implemented corresponding functions in DemoModulo.py"

--- Python/05-Microservicios/Ejercicios/DemoApp.py
+++ Python/05-Microservicios/Ejercicios/DemoApp.py
@@ -16,11 +16,32 @@ def get_customers():
     # return Response(Get_Customers_List(), status=200, content_type="application/json")
     return jsonify(Get_Customers_List()), 200
 
+
 @app.route("/api/customers/<id>", methods=["GET"])
 def get_customer(id):
     # return Response(Get_Customer(id), status=200, content_type="application/json")
     return jsonify(Get_Customer(id)), 200
 
+
+
+# PARA HACER:
+
+
+@app.route("/api/orders", methods=["GET"])
+def get_orders():
+    # return Response(Get_Orders_List(), status=200, content_type="application/json")
+    return jsonify(Get_Orders_List()), 200
+
+@app.route("/api/orders/<id>", methods=["GET"])
+def get_order(id):
+    # return Response(Get_Order(id), status=200, content_type="application/json")
+    return jsonify(Get_Order(id)), 200
+
+
+@app.route("/api/customers/<id>/orders", methods=["GET"])
+def get_customer_orders(id):
+    # return Response(Get_Customer_Orders(id), status=200, content_type="application/json")
+    return jsonify(Get_Customer_Orders(id)), 200
 
 #########################################################################
 # Control de errores aplicación Flask
@@ -46,3 +67,8 @@ def handle_error(e):
 # Ejecutar la aplicación de Flask en el servidor web integrado
 #########################################################################
 app.run()
+
+
+
+# httprepl http://127.0.0.1:5000
+# esto es en la terminal, para printear como si fuera el mongo pero ahí
--- Python/05-Microservicios/Ejercicios/DemoModulo.py
+++ Python/05-Microservicios/Ejercicios/DemoModulo.py
@@ -19,6 +19,31 @@ def Get_Customer(id) -> str:
     except Exception as e:
         return {"Error": e}
 
-
 def _Get_Collection(collection, server=server, port=port, db=db):
     return MongoClient(f"mongodb://{server}:{port}/")[db][collection]
+
+
+# PARA HCER
+
+
+def Get_Orders_List() -> str:
+    try:
+        # return dumps(list(_Get_Collection("orders").find({}))
+        return list(_Get_Collection("orders").find({}, {"_id": 0}))
+    except Exception as e:
+        return {"Error": e}
+    
+def Get_Order(id) -> str:
+    try:
+        # return dumps(_Get_Collection("orders").find_one({"OrderID": id}))
+        return _Get_Collection("orders").find_one({"OrderID": id}, {"_id": 0})
+    except Exception as e:
+        return {"Error": e}
+    
+    
+def Get_Customer_Orders(id) -> list:
+    try:
+        # return dumps(list(_Get_Collection("orders").find_one({"CustomerID": id})))
+        return list(_Get_Collection("orders").find({"CustomerID": id}, {"_id": 0}))
+    except Exception as e:
+        return {"Error": e}
